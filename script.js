19045d54f190021a81a63037a73d7b58d2a2aa65
Marek Drda <drda.marek@gmail.com>
2022-10-17 13:37:43 +0200

    Created a constant of message, writing down the prophecy in script.js.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Source strings for messages
 const barbSigns = ["oak", "boulder", "fire", "war", "hammer", "fist", "thunder", "horse", "blood", "bear", "smoke", "bone"]
-const shouldOrShouldnt = ["should", "shouldn't"];
+const modal = ["should", "shouldn't", "will", "will not", "must", "must not"];
 const verb = ["find", "leave", "fear", "protect", "fight", "teach", "kiss", "kill", "strike", "protect", "hunt"];
 const person = ["witch", "man", "hunter", "dog", "dragon", "king", "queen", "bastard", "child", "snail", "frog", "raven", "old woman", "oracle"];
 const time = ["today", "this week", "very soon", "next year", "one day"];
@@ -9,12 +9,7 @@ const time = ["today", "this week", "very soon", "next year", "one day"];
 const getRandom = arr => {
     return arr[Math.floor(Math.random() * arr.length)];
 }
-console.log(getRandom(verb));
-console.log(getRandom(verb));
-console.log(getRandom(verb));
-console.log(getRandom(time));
-console.log(getRandom(time));
-console.log(getRandom(time));
-console.log(getRandom(person));
-console.log(getRandom(shouldOrShouldnt));
-console.log(getRandom(barbSigns));
+
+// Write the message
+let message = `Your barbaric sign is ${getRandom(barbSigns)}. You ${getRandom(modal)} ${getRandom(verb)} the ${getRandom(person)} ${getRandom(time)}.`;
+console.log(message);
